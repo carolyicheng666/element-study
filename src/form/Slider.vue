3a6375fb2c7dbe02cddc4d907f8cb899308cf38b
carolyicheng666 <790496165@qq.com>
2018-01-08 17:51:45 +0800

    form

---
+++ src/form/Slider.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <h1>Slider 滑块</h1>
+    <p>通过拖动滑块在一个固定区间内进行选择</p>
+
+    <h2>基础用法</h2>
+    <p>在拖动滑块时，显示当前值</p>
+    <div class="block">
+      <span class="demonstration">默认</span>
+      <el-slider v-model="value1"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">自定义初始值</span>
+      <el-slider v-model="value2"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">隐藏 Tooltip</span>
+      <el-slider v-model="value3" :show-tooltip="false"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">格式化 Tooltip</span>
+      <el-slider v-model="value4" :format-tooltip="formatTooltip"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">禁用</span>
+      <el-slider v-model="value5" disabled></el-slider>
+    </div>
+
+    <h2>离散值</h2>
+    <p>选项可以是离散的。改变step的值可以改变步长，通过设置 show-stops 属性可以显示间断点</p>
+    <div class="block">
+      <span class="demonstration">不显示间断点</span>
+      <el-slider v-model="value6" :step="10"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">显示间断点</span>
+      <el-slider v-model="value7" :step="10" show-stops></el-slider>
+    </div>
+
+    <h2>带有输入框</h2>
+    <p>通过输入框设置精确数值。设置show-input属性会在右侧显示一个输入框</p>
+    <div class="block">
+      <el-slider v-model="value8" show-input></el-slider>
+    </div>
+
+    <h2>范围选择</h2>
+    <p>支持选择某一数值范围。设置range即可开启范围选择，此时绑定值是一个数组，其元素分别为最小边界值和最大边界值</p>
+    <div class="block">
+      <el-slider v-model="value9" range show-stops :max="10"></el-slider>
+    </div>
+
+    <h2>竖向模式</h2>
+    <p>设置vertical可使 Slider 变成竖向模式，此时必须设置高度height属性</p>
+    <div class="block">
+      <el-slider v-model="value9" vertical height="200px"></el-slider>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        value1: 0,
+        value2: 50,
+        value3: 36,
+        value4: 48,
+        value5: 42,
+        value6: 0,
+        value7: 0,
+        value8: 0,
+        value9: [4, 8],
+        value10: 0
+      }
+    },
+    methods: {
+      formatTooltip(val) {
+        return val / 100;
+      }
+    }
+  }
+</script>
---
+++ src/form/TimePicker.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>TimePicker 时间选择器</h1>
+    <p>用于选择或输入日期</p>
+
+    <h2>固定时间点</h2>
+    <p>提供几个固定的时间点供用户选择。使用 el-time-select 标签，分别通过star、end和step指定可选的起始时间、结束时间和步长</p>
+    <el-time-select v-model="value1" :picker-options="{ start: '08:30', step: '00:15', end: '18:30' }" placeholder="选择时间"></el-time-select>
+
+    <h2>任意时间点</h2>
+    <p>可以选择任意时间。使用 el-time-picker 标签，通过selectableRange限制可选时间范围。提供了两种交互方式：默认情况下通过鼠标滚轮进行选择，打开arrow-control属性则通过界面上的箭头进行选择。</p>
+    <el-time-picker v-model="value2" :picker-options="{ selectableRange: '18:30:00 - 20:30:00' }" placeholder="任意时间点"></el-time-picker>
+    <el-time-picker v-model="value3" arrow-control :picker-options="{ selectableRange: '18:30:00 - 20:30:00' }" placeholder="任意时间点"></el-time-picker>
+
+    <h2>固定时间范围</h2>
+    <p>若先选择开始时间，则结束时间内备选项的状态会随之改变</p>
+    <el-time-select v-model="startTime" :picker-options="{ start: '08:30', step: '00:15', end: '18:30' }" placeholder="起始时间"></el-time-select>
+    <el-time-select v-model="endTime" :picker-options="{ start: '08:30', step: '00:15', end: '18:30', minTime: startTime }" placeholder="结束时间"></el-time-select>
+
+    <h2>任意时间范围</h2>
+    <p>可选择任意的时间范围。添加is-range属性即可选择时间范围，同样支持arrow-control属性。</p>
+    <el-time-picker is-range v-model="value4" range-separator="至" start-placeholder="开始时间" end-placeholder="结束时间" placeholder="选择时间范围"></el-time-picker>
+    <el-time-picker is-range v-model="value5" arrow-control range-separator="至" start-placeholder="开始时间" end-placeholder="结束时间" placeholder="选择时间范围"></el-time-picker>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        value1: '',
+        value2: new Date(2016, 9, 10, 18, 40),
+        value3: new Date(2016, 9, 10, 18, 40),
+        startTime: '',
+        endTime: '',
+        value4: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
+        value5: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)]
+      };
+    }
+  }
+</script>
