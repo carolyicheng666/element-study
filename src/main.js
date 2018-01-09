e47161a59dcb3812bab49988041e2ecf6eb6d1b0
carolyicheng666 <790496165@qq.com>
2018-01-09 11:43:26 +0800

    form

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import App from './form/DateTimePicker.vue'
+import App from './form/Form.vue'
 
 Vue.use(ElementUI)
 
