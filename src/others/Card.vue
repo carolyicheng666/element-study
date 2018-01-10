77a54c305980b8dff5a6c9bdcd00615b3f8875e7
carolyicheng666 <790496165@qq.com>
2018-01-10 17:44:16 +0800

    others

---
+++ src/others/Card.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+    <h1>Card 卡片</h1>
+    <p>将信息聚合在卡片容器中展示。</p>
+
+    <h2>基础用法</h2>
+    <p>包含标题，内容和操作。Card 组件包括header和body部分，header部分需要有显式具名 slot 分发，同时也是可选的。</p>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>卡片名称</span>
+        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+      </div>
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容 ' + o }}
+      </div>
+    </el-card>
+
+    <h2>简单卡片</h2>
+    <p>卡片可以只有内容区域。</p>
+    <el-card class="box-card">
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容 ' + o }}
+      </div>
+    </el-card>
+    
+    <h2>带图片</h2>
+    <p>可配置定义更丰富的内容展示。配置body-style属性来自定义body部分的style，我们还使用了布局组件。</p>
+    <el-row>
+      <el-col :span="3" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="../assets/logo.png" class="image">
+          <div style="padding: 14px;">
+            <span>好吃的汉堡</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ currentDate }}</time>
+              <el-button type="text" class="button">操作按钮</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      currentDate: new Date()
+    };
+  }
+}
+</script>
+<style>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
+  }
+
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+</style>
---
+++ src/others/Carousel.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <h1>Carousel 走马灯</h1>
+    <p>在有限空间内，循环播放同一类型的图片、文字等内容</p>
+
+    <h2>基础用法</h2>
+    <p>适用广泛的基础用法。结合使用el-carousel和el-carousel-item标签就得到了一个走马灯。幻灯片的内容是任意的，需要放在el-carousel-item标签中。默认情况下，在鼠标 hover 底部的指示器时就会触发切换。通过设置trigger属性为click，可以达到点击触发的效果。</p>
+    <div class="block">
+      <span class="demonstration">默认 Hover 指示器触发</span>
+      <el-carousel height="150px">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="block">
+      <span class="demonstration">Click 指示器触发</span>
+      <el-carousel trigger="click" height="150px">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+
+    <h2>指示器</h2>
+    <p>可以将指示器的显示位置设置在容器外部。indicator-position属性定义了指示器的位置。默认情况下，它会显示在走马灯内部，设置为outside则会显示在外部；设置为none则不会显示指示器。</p>
+    <el-carousel indicator-position="outside" height="150px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+
+    <h2>切换箭头</h2>
+    <p>可以设置切换箭头的显示时机。arrow属性定义了切换箭头的显示时机。默认情况下，切换箭头只有在鼠标 hover 到走马灯上时才会显示；若将arrow设置为always，则会一直显示；设置为never，则会一直隐藏。</p>
+    <el-carousel :interval="5000" arrow="always" height="150px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+
+    <h2>卡片化</h2>
+    <p>当页面宽度方向空间空余，但高度方向空间匮乏时，可使用卡片风格。将type属性设置为card即可启用卡片模式。从交互上来说，卡片模式和一般模式的最大区别在于，可以通过直接点击两侧的幻灯片进行切换。</p>
+    <el-carousel :interval="4000" type="card" height="150px">
+    <el-carousel-item v-for="item in 6" :key="item">
+      <h3>{{ item }}</h3>
+    </el-carousel-item>
+  </el-carousel>
+  </div>
+</template>
+<style>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+    text-align: center;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
+  }
+</style>
