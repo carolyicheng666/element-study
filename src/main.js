19fa2d460b1a3553dc2a7fc06aaa5d73ef5918fd
carolyicheng666 <790496165@qq.com>
2018-01-11 10:45:46 +0800

    更新Element至v2.0.11

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import App from './others/Collapse.vue'
+import App from './data/Table.vue'
 
 Vue.use(ElementUI)
 
